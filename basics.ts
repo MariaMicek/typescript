a77fb96356e8db6f0c21779a936c1339a09e35ce
Maria Micek <mmicek16@wp.pl>
2019-04-24 11:44:02 +0200

    array with one type of elements' values

--- basics.ts
+++ basics.ts
@@ -1,5 +1,10 @@
 let zrobione: boolean = true
 let liczba: number = 123
 let tekst: string = 'to jest tekst'
-let wnioskowanie = 'test inferencji'
-wnioskowanie = 123
+
+let tablicaLiczb: number[] = [1,2,3,4,5]  
+let tablicaLiczb2: Array<number> = [1,2,3]
+
+let tablicaStringow: string[] = ['ala', 'ma', 'kota']
+let tablicaStringow2: Array<string> = ['ok', 2]
+tablicaStringow2.push(123)
