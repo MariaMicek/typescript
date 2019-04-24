44e105daf4165e488fa0f0af1136526bc483ea41
Maria Micek <mmicek16@wp.pl>
2019-04-24 11:49:27 +0200

    tuple and nested tuples

--- basics.ts
+++ basics.ts
@@ -2,9 +2,14 @@ let zrobione: boolean = true
 let liczba: number = 123
 let tekst: string = 'to jest tekst'
 
-let tablicaLiczb: number[] = [1,2,3,4,5]  
-let tablicaLiczb2: Array<number> = [1,2,3]
+let tablicaLiczb: number[] = [1, 2, 3, 4, 5]
+let tablicaLiczb2: Array<number> = [1, 2, 3]
 
 let tablicaStringow: string[] = ['ala', 'ma', 'kota']
 let tablicaStringow2: Array<string> = ['ok', 2]
 tablicaStringow2.push(123)
+
+let krotka1: [number, boolean] = [12, true]
+krotka1 = ['a', 12]
+let krotka2: [string, boolean[]] = ['test', [true, false]]
+let krotka3: [[number, boolean], [string, boolean[]]] = [krotka1, krotka2]
