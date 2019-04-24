50786bcef58345aaf29ba850b7720e2c65627143
Maria Micek <mmicek16@wp.pl>
2019-04-24 12:04:44 +0200

    enum

--- basics.ts
+++ basics.ts
@@ -13,3 +13,19 @@ let krotka1: [number, boolean] = [12, true]
 krotka1 = ['a', 12]
 let krotka2: [string, boolean[]] = ['test', [true, false]]
 let krotka3: [[number, boolean], [string, boolean[]]] = [krotka1, krotka2]
+
+
+let miesiacRezerwacji: string = 'lipiec'
+miesiacRezerwacji = 'june'
+
+if(miesiacRezerwacji === 'lipiec'){
+    //fuckup
+}
+
+enum Miesiac {STYCZEN, LUTY, MARZEC, KWIECIEN, MAJ, CZERWIEC, LIPIEC, SIERPIEN, WRZESIEN, PAZDZIERNIK, LISTOPAD, GRUDZIEN}
+
+let newMiesiacRezerwacji: Miesiac = Miesiac.LIPIEC 
+
+if (newMiesiacRezerwacji === Miesiac.LIPIEC) {
+    console.log('sukces')
+}
