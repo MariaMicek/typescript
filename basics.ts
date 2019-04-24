83348403bd2094557566f0da78b85d1a2ca45265
Maria Micek <mmicek16@wp.pl>
2019-04-24 14:00:12 +0200

    interface

--- basics.ts
+++ basics.ts
@@ -1,30 +1,21 @@
-//OBJECT
-let mojObiekt: object = {}
-mojObiekt = undefined
-mojObiekt = 123
+//INTERFACE
 
-//FUNCTIONS
-const func = (a: number, b: number = 2, c?: number) => {                       //parametry zwykłe są wymagane        
-    return a + b + (c ? c : 0)                                                 //parametry zwykłe, z wartością domyślną i opcjonalne
+interface NaszTyp {
+    imie: string;
+    wiek: number;
+    dodatkowe?: boolean;
 }
 
-const value1 = func()
-const value2 = func(2)
-const value3 = func(2, 3)
-const value4 = func(2, 3, 4)
-const value5 = func('abc')
-
-let result: string
-result = value2
-
-
-enum Weekend {FRIDAY, SATURDAY, SUNDAY}
-
-const myFunction = (a: [number, boolean], b: Weekend, c: number = 20, d?: boolean, e?: boolean): Array<Weekend> => {
-    return [Weekend.FRIDAY, Weekend.SATURDAY, Weekend.SUNDAY]
+function testFunc(param: NaszTyp): void {
+    console.log(param.imie)
+    console.log(param.wiek)
+    console.log(param.dodatkowe)
 }
 
-const v1 = myFunction([10, true], Weekend.FRIDAY)
-const v2 = myFunction([10, true], Weekend.FRIDAY, 20)
-const v3 = myFunction([10, true], Weekend.FRIDAY, 20, true)
-const v4 = myFunction([10, true], Weekend.FRIDAY, 20, true, false)
+let testObj = {
+    imie: 'marysia',
+    wiek: 21,
+    dodatkowe: true
+}
+
+testFunc(testObj)
