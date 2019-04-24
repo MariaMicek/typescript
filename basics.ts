2cbe84fd72d722e876de1efebaf6aa44fe13f019
Maria Micek <mmicek16@wp.pl>
2019-04-24 11:35:42 +0200

    typescript is checking types of variables even if we do not specify it

--- basics.ts
+++ basics.ts
@@ -1,7 +1,5 @@
 let zrobione: boolean = true
-
 let liczba: number = 123
-liczba = '123'              //javascript i tak się wykona, ale dostajemy błąd w vs code
-console.log(liczba)
-
 let tekst: string = 'to jest tekst'
+let wnioskowanie = 'test inferencji'
+wnioskowanie = 123
