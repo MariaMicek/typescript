d83f545eae93c71a6cfa379972d3b784c32ccb0e
Maria Micek <mmicek16@wp.pl>
2019-04-24 11:32:24 +0200

    assigning wrong value to variable

---
+++ basics.ts
@@ -0,0 +1,7 @@
+let zrobione: boolean = true
+
+let liczba: number = 123
+liczba = '123'              //javascript i tak się wykona, ale dostajemy błąd w vs code
+console.log(liczba)
+
+let tekst: string = 'to jest tekst'
